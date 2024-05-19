b9a26cd4114bc94843f91634fbc8ffe33fd6fb01
Markus <marcosbh2002bh@gmail.com>
2024-05-19 22:06:07 +0200

    Added nav and header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import Header from "./components/Header/Header.jsx";
+import Nav from "./components/Nav/Nav.jsx";
+
 function App() {
 
   return (
     <>
-      <h1> App rendering</h1>
+        <Header />
+        <main>
+            <aside>
+                <Nav />
+            </aside>
+            <section>
+                <h2>Content</h2>
+            </section>
+        </main>
     </>
   )
 }
