a67750965887dae9895cad0fd0829dd0d0111895
Markus <marcosbh2002bh@gmail.com>
2024-05-20 10:52:42 +0200

    Formatted with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import Header from './components/Header/Header.jsx'
 import Nav from './components/Nav/Nav.jsx'
 
 function App() {
-    return (
-        <>
-            <Header />
-            <aside>
-                <Nav />
-            </aside>
-            <main>
-                <section>
-                    <h2>Content</h2>
-                </section>
-            </main>
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <aside>
+        <Nav />
+      </aside>
+      <main>
+        <section>
+          <h2>Content</h2>
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default App
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,52 +5,52 @@ import NavExpanded from '../../icons/NavExpanded.jsx'
 import styles from './Nav.module.css'
 
 function Nav() {
-    const inputID = useId()
-    const languageSelector = useId()
-    const themeSelector = useId()
+  const inputID = useId()
+  const languageSelector = useId()
+  const themeSelector = useId()
 
-    const [navExpanded, setNavExpanded] = useState(false)
+  const [navExpanded, setNavExpanded] = useState(false)
 
-    return (
-        <nav className={styles.nav}>
-            <label htmlFor={inputID} className={styles.navCheckBox}>
-                {navExpanded ? (
-                    <NavExpanded width={'5rem'} />
-                ) : (
-                    <NavCollapsed width={'5rem'} />
-                )}
-            </label>
-            <input
-                type={'checkbox'}
-                id={inputID}
-                checked={navExpanded}
-                className={styles.navCheckBoxInput}
-                onChange={() => {
-                    setNavExpanded(!navExpanded)
-                }}
-            />
-            <div className={styles.dropDownMenu}>
-                <fieldset>
-                    <legend>Settings</legend>
-                    <label htmlFor={languageSelector}>
-                        Language:
-                        <select>
-                            <option value={'en'}>English</option>
-                            <option value={'es'}>Español</option>
-                        </select>
-                    </label>
+  return (
+    <nav className={styles.nav}>
+      <label htmlFor={inputID} className={styles.navCheckBox}>
+        {navExpanded ? (
+          <NavExpanded width={'5rem'} />
+        ) : (
+          <NavCollapsed width={'5rem'} />
+        )}
+      </label>
+      <input
+        type={'checkbox'}
+        id={inputID}
+        checked={navExpanded}
+        className={styles.navCheckBoxInput}
+        onChange={() => {
+          setNavExpanded(!navExpanded)
+        }}
+      />
+      <div className={styles.dropDownMenu}>
+        <fieldset>
+          <legend>Settings</legend>
+          <label htmlFor={languageSelector}>
+            Language:
+            <select>
+              <option value={'en'}>English</option>
+              <option value={'es'}>Español</option>
+            </select>
+          </label>
 
-                    <label htmlFor={themeSelector}>
-                        Theme:
-                        <select>
-                            <option value={'light'}>Light</option>
-                            <option value={'dark'}>Dark</option>
-                        </select>
-                    </label>
-                </fieldset>
-            </div>
-        </nav>
-    )
+          <label htmlFor={themeSelector}>
+            Theme:
+            <select>
+              <option value={'light'}>Light</option>
+              <option value={'dark'}>Dark</option>
+            </select>
+          </label>
+        </fieldset>
+      </div>
+    </nav>
+  )
 }
 
 export default Nav
