970f8fbeaa73c8b44bcdedcdfe75c10a747f7a67
Markus <marcosbh2002bh@gmail.com>
2024-05-21 22:15:00 +0200

    CSS upgraded and added title to seeTasks

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types'
 
+import styles from './Task.module.css'
+
 function Task({ task }) {
   return (
-    <article>
+    <article className={styles.task}>
       <h2>{task.title}</h2>
       <p>{task.body}</p>
     </article>
--- src/pages/SeeTasks/SeeTasks.jsx
+++ src/pages/SeeTasks/SeeTasks.jsx
@@ -1,11 +1,13 @@
 import { Task } from '../../components/Task/Task.jsx'
 import { useTasks } from '../../hooks/useTasks.js'
+import styles from './SeeTasks.module.css'
 
 function SeeTasks() {
   const { tasks } = useTasks()
 
   return (
-    <main>
+    <main className={styles.main}>
+      <h1>Pendent Tasks</h1>
       {tasks.map((task) => (
         <Task task={task} key={task.id} />
       ))}
