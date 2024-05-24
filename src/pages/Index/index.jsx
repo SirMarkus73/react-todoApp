c6c0a31e21cb1829b71ac2911339ae240984b1ec
Markus <marcosbh2002bh@gmail.com>
2024-05-24 19:13:25 +0200

    Upgraded index

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,7 +1,12 @@
 import styles from './Index.module.css'
 
 function Index() {
-  return <main className={styles.index}>Esto es el indice</main>
+  return (
+    <main className={styles.index}>
+      <h1>Welcome to my ToDo App!</h1>
+      <p>Start managing your tasks and stay organized.</p>
+    </main>
+  )
 }
 
 export { Index }
