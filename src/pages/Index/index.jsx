ff2bc84462a8d0c65c6c948abf76db74619b3a04
Markus <marcosbh2002bh@gmail.com>
2024-05-24 19:03:37 +0200

    Created new CSS file and modified index.jsx

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,5 +1,7 @@
+import styles from './Index.module.css'
+
 function Index() {
-  return <main>Esto es el indice</main>
+  return <main className={styles.main}>Esto es el indice</main>
 }
 
 export { Index }
