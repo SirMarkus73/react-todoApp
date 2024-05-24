59c7ca22956258b23d6fe2b1a7bf43b093e09d0e
Markus <marcosbh2002bh@gmail.com>
2024-05-24 19:08:57 +0200

    Grammar error in src/global-styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
-import './global-syles/index.css'
+import './global-styles/index.css'
 import { PageContextProvider } from './providers/PageContextProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,7 +1,7 @@
 import styles from './Index.module.css'
 
 function Index() {
-  return <main className={styles.main}>Esto es el indice</main>
+  return <main className={styles.index}>Esto es el indice</main>
 }
 
 export { Index }
