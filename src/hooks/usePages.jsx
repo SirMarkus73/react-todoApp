20518b4f6828edaa3d293ce832eda8055ebcfa6e
Markus <marcosbh2002bh@gmail.com>
2024-05-24 21:15:19 +0200

    refactor: Update import statement for Index component in usePages hook

--- src/hooks/usePages.jsx
+++ src/hooks/usePages.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect } from 'react'
 
 import { pages } from '../constants/pages.js'
 import { CreateTask } from '../pages/CreateTask/CreateTask.jsx'
-import { Index } from '../pages/Index/index.jsx'
+import { Index } from '../pages/Index/Index.jsx'
 import { SeeTasks } from '../pages/SeeTasks/SeeTasks.jsx'
 import { PageContext } from '../providers/PageContextProvider.jsx'
 
